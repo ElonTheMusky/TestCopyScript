8023af51c2cd86a20c5cab6fe0a25c8c13c166d3
Flight Crew <flight.crew@heliolytics.com>
2022-07-15 16:14:13 -0400

    first working version

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-SOURCE_FILE_PATH = r"D:\drone_data"
-DESTINATION_FILE_PATH = r"C:\Users\Andrew Guo\Documents\Work\ProtoProjectHashImages"
-USB_DRIVE_CODE = r"D:"
+SOURCE_FILE_PATH = r"H:\drone_data"
+DESTINATION_FILE_PATH = r"I:\Data"
+USB_DRIVE_CODE = r"H:"
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import shutil
 import glob
 import os
 import re
+from tkinter import image_names
 
 from constants import SOURCE_FILE_PATH, DESTINATION_FILE_PATH, USB_DRIVE_CODE
 from os import listdir
@@ -11,6 +12,7 @@ class HashingError(Exception):
     pass
 
 def hash_image(image):
+    assert os.path.isfile(image)
     with open(image,"rb") as f:
         bro = f.read()
         jim = hashlib.sha256(bro).hexdigest();
@@ -20,18 +22,20 @@ def check_images(src_file_path):
     file_names = listdir(os.path.join(USB_DRIVE_CODE, src_file_path))
     # print(file_names)
     for name in file_names:
-        src_hash = hash_image(os.path.join(USB_DRIVE_CODE, src_file_path, name))
-        shutil.copyfile(os.path.join(USB_DRIVE_CODE, src_file_path, name), os.path.join(DESTINATION_FILE_PATH, src_file_path))
-        dst_hash = hash_image(os.path.join(DESTINATION_FILE_PATH, src_file_path, name))
-        if(src_hash != dst_hash):
-            raise HashingError("Error! Something went wrong: " + os.path.join(DESTINATION_FILE_PATH, src_file_path, name))
+        if(os.path.isfile(os.path.join(USB_DRIVE_CODE, src_file_path, name))):
+            src_hash = hash_image(os.path.join(USB_DRIVE_CODE, src_file_path, name))
+            shutil.copy(os.path.join(USB_DRIVE_CODE, src_file_path, name), os.path.join(DESTINATION_FILE_PATH, src_file_path))
+            dst_hash = hash_image(os.path.join(DESTINATION_FILE_PATH, src_file_path, name))
+            if(src_hash != dst_hash):
+                raise HashingError("Error! Something went wrong: " + os.path.join(DESTINATION_FILE_PATH, src_file_path, name))
 
 def locate_scan_folders():
     # Looking for .jpg file types!
     folder_locations = []
     path = os.path.join(SOURCE_FILE_PATH, '\\**\\*.jpg')
     # print(path)
-    image_locations = glob.glob(path, recursive=True)  
+    image_locations = glob.glob(path, recursive=True)
+    # print(image_locations) 
     for loc in image_locations:
         loc_str = loc.split("\\")
         loc_str.pop(len(loc_str) - 1)
@@ -55,9 +59,11 @@ def locate_scan_folders():
 def make_copies():
     # locate_scan_folders()
     # print(SOURCE_FILE_PATH)
-    # print(sorted(locate_scan_folders(SOURCE_FILE_PATH)))
+    # print(sorted(locate_scan_folders()))
     for loc in sorted(locate_scan_folders()):
-        print("Location: " + loc)
+        # print("Location: " + loc)
         check_images(loc)
 
 make_copies()
+
+# "H:\drone_data\36867 - Washington 1 - 2022-04-05\overview\Overview\1\110MEDIA\DJI_0504_R.JPG"
