6b3535771e057b30979033e22f5bf75e6f32632f
Andrew-Ghee-UO <47193626+Andrew-Ghee-UO@users.noreply.github.com>
2022-07-15 14:25:36 -0400

    minor changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import hashlib
 import shutil
 import glob
 import os
+import re
 
 from constants import SOURCE_FILE_PATH, DESTINATION_FILE_PATH, USB_DRIVE_CODE
 from os import listdir
@@ -16,41 +17,46 @@ def hash_image(image):
     return jim
         
 def check_images(src_file_path):
-    file_names = listdir(USB_DRIVE_CODE + "\\" + src_file_path)
-    print(file_names)
+    file_names = listdir(os.path.join(USB_DRIVE_CODE, src_file_path))
+    # print(file_names)
     for name in file_names:
-        src_hash = hash_image(USB_DRIVE_CODE + "\\" + src_file_path + "\\" + name)
-        shutil.copyfile(USB_DRIVE_CODE + "\\" +src_file_path + "\\" + name, DESTINATION_FILE_PATH + "\\" + src_file_path)
-        dst_hash = hash_image(DESTINATION_FILE_PATH + "\\" + src_file_path + "\\" + name)
+        src_hash = hash_image(os.path.join(USB_DRIVE_CODE, src_file_path, name))
+        shutil.copyfile(os.path.join(USB_DRIVE_CODE, src_file_path, name), os.path.join(DESTINATION_FILE_PATH, src_file_path))
+        dst_hash = hash_image(os.path.join(DESTINATION_FILE_PATH, src_file_path, name))
         if(src_hash != dst_hash):
-            raise HashingError("Error! Something went wrong: " + DESTINATION_FILE_PATH + "\\" + src_file_path + "\\" + name)
+            raise HashingError("Error! Something went wrong: " + os.path.join(DESTINATION_FILE_PATH, src_file_path, name))
 
-def locate_scan_folders(file_path):
+def locate_scan_folders():
     # Looking for .jpg file types!
     folder_locations = []
-    image_locations = glob.glob(file_path + '/**/*.jpg', recursive=True)  
+    path = os.path.join(SOURCE_FILE_PATH, '\\**\\*.jpg')
+    # print(path)
+    image_locations = glob.glob(path, recursive=True)  
     for loc in image_locations:
         loc_str = loc.split("\\")
         loc_str.pop(len(loc_str) - 1)
+        # print("loc_str: " + str(loc_str))
+        # print("loc: " + loc)
         loc_str.pop(0)
         new_loc_str = ""
         index = 0
         while(True):
-            new_loc_str += loc_str[index]
-            if not (os.path.exists(DESTINATION_FILE_PATH + "\\" + new_loc_str)):
-                os.mkdir(DESTINATION_FILE_PATH + "\\" + new_loc_str)
+            new_loc_str = os.path.join(new_loc_str, loc_str[index])
+            if not (os.path.exists(os.path.join(DESTINATION_FILE_PATH, new_loc_str))):
+                os.mkdir(os.path.join(DESTINATION_FILE_PATH, new_loc_str))
                 break
             if(index >= len(loc_str) - 1):
                 break
-            new_loc_str += "\\"
             index += 1
         folder_locations.append(new_loc_str)
     locations_set = set(folder_locations)
     return (list(locations_set))
 
 def make_copies():
+    # locate_scan_folders()
+    # print(SOURCE_FILE_PATH)
     # print(sorted(locate_scan_folders(SOURCE_FILE_PATH)))
-    for loc in sorted(locate_scan_folders(SOURCE_FILE_PATH)):
+    for loc in sorted(locate_scan_folders()):
         print("Location: " + loc)
         check_images(loc)
 
